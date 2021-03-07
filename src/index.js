10f4591d39031c864eedc0008860554dfc451dae
luisfalconmx <me@luisfalconmx.com>
2021-03-06 23:32:34 -0600

    feat: initial cli questions configured with inquirir js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,55 @@
-const hello = () => {
-  console.log("hello world");
-};
+const inquirer = require("inquirer");
 
-module.exports = { hello };
+inquirer
+  .prompt([
+    {
+      name: "project",
+      message: "What is the name of your project?",
+      default: "webkit",
+    },
+    {
+      name: "views",
+      type: "list",
+      message: "What will you use to create the views?",
+      choices: ["html", "pug"],
+    },
+    {
+      name: "framework",
+      type: "list",
+      message: "What css framework will you use?",
+      choices: ["none", "tailwindcss", "bootstrap 5"],
+    },
+    {
+      name: "styles",
+      type: "list",
+      message: "What css preprocessor will you use?",
+      choices: ["none", "postcss", "sass"],
+      when: (answers) => answers.framework === "none",
+    },
+    {
+      name: "linters",
+      type: "checkbox",
+      message: "What linters will you use in your project?",
+      choices: [
+        {
+          name: "ESLint",
+          checked: true,
+        },
+        {
+          name: "Prettier",
+          checked: true,
+        },
+        {
+          name: "Stylelint",
+          checked: false,
+        },
+        {
+          name: "CommitLint",
+          checked: false,
+        },
+      ],
+    },
+  ])
+  .then((answers) => {
+    console.log("Answers: ", answers);
+  });
