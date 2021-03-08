4fd275cccc28978b1a3ab744cc51cf47cf378048
luisfalconmx <me@luisfalconmx.com>
2021-03-07 20:02:33 -0600

    refactor: prompt module improved and options set in cli

--- bin/cli.js
+++ bin/cli.js
@@ -1,9 +1,33 @@
 #!/usr/bin/env node
 const prompt = require("../src/utils/prompt.js");
 
-const getAnswers = async () => {
-  const answers = await prompt.init;
-  console.log(answers);
-};
+const questions = prompt.setQuestions([
+  {
+    name: "project",
+    message: "What is the name of your project?",
+    default: "webkit",
+  },
+  {
+    name: "views",
+    type: "list",
+    message: "What will you use to create the views?",
+    choices: ["html", "pug"],
+  },
+  {
+    name: "framework",
+    type: "list",
+    message: "What css framework will you use?",
+    choices: ["none", "tailwindcss", "bootstrap 5"],
+  },
+  {
+    name: "styles",
+    type: "list",
+    message: "What css preprocessor will you use?",
+    choices: ["none", "postcss", "sass"],
+    when: (answers) => answers.framework === "none",
+  },
+]);
 
-getAnswers();
+prompt.getAnswers(questions).then((answers) => {
+  console.log(answers);
+});
--- src/utils/prompt.js
+++ src/utils/prompt.js
@@ -1,34 +1,14 @@
 const inquirer = require("inquirer");
 
-const init = inquirer
-  .prompt([
-    {
-      name: "project",
-      message: "What is the name of your project?",
-      default: "webkit",
-    },
-    {
-      name: "views",
-      type: "list",
-      message: "What will you use to create the views?",
-      choices: ["html", "pug"],
-    },
-    {
-      name: "framework",
-      type: "list",
-      message: "What css framework will you use?",
-      choices: ["none", "tailwindcss", "bootstrap 5"],
-    },
-    {
-      name: "styles",
-      type: "list",
-      message: "What css preprocessor will you use?",
-      choices: ["none", "postcss", "sass"],
-      when: (answers) => answers.framework === "none",
-    },
-  ])
-  .then((answers) => {
+function setQuestions(questions) {
+  return inquirer.prompt(questions).then((answers) => {
     return answers;
   });
+}
 
-module.exports = { init };
+async function getAnswers(promise) {
+  const answers = await promise;
+  return answers;
+}
+
+module.exports = { setQuestions, getAnswers };
