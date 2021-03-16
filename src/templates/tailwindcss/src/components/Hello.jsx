d5f5a325277e947ba04efbdac2a6a6c16a6a20c4
luisfalconmx <me@luisfalconmx.com>
2021-03-15 20:02:00 -0600

    feat: eslint and prettier configured with react airbnb

--- src/templates/tailwindcss/src/components/Hello.jsx
+++ src/templates/tailwindcss/src/components/Hello.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 
 import Logo from '@images/github-logo.png'
-import LogoWebp from '@images/github-logo.png?format=webp'
 
 const Hello = () => (
   <div>
     <h1>Hello World</h1>
     <picture>
-      <source type="image/webp" srcSet={LogoWebp} />
+      <source type="image/webp" />
       <source type="image/jpeg" srcSet={Logo} />
       <img src={Logo} alt="" />
     </picture>
