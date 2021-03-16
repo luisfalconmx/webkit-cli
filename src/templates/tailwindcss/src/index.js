2f1c8c54336754a3a89960a9e8a69f9cf8c04550
luisfalconmx <me@luisfalconmx.com>
2021-03-15 18:10:11 -0600

    refactor: the code of the webpack file was improved to make it clean

--- src/templates/tailwindcss/src/index.js
+++ src/templates/tailwindcss/src/index.js
@@ -2,14 +2,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+// Styles
+import '@styles/library/tailwind.base.pcss'
+import '@styles/loader.base.pcss'
+import '@styles/library/tailwind.components.pcss'
+import '@styles/loader.components.pcss'
+import '@styles/library/tailwind.utilities.pcss'
+
 // Components
 import App from './containers/App'
-
-// Styles
-import './styles/loader.base.pcss'
-import './styles/library/tailwind.base.pcss'
-import './styles/library/tailwind.components.pcss'
-import './styles/loader.components.pcss'
-import './styles/library/tailwind.utilities.pcss'
 
 ReactDOM.render(<App />, document.getElementById('app'))
--- src/templates/tailwindcss/webpack.config.js
+++ src/templates/tailwindcss/webpack.config.js
@@ -8,152 +8,110 @@ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPl
 
 const config = {}
 
-config.entry = './src/index.js'
-
-config.output = {
-  path: path.resolve(__dirname, 'dist'),
-  filename: '[name].[contenthash].js',
-  assetModuleFilename: 'assets/images/[hash][ext]'
-}
-
-config.resolve = {
-  extensions: ['.js', '.jsx'],
-  alias: {
-    '@styles': path.resolve(__dirname, 'src/styles/'),
-    '@images': path.resolve(__dirname, 'src/assets/images/'),
-    '@fonts': path.resolve(__dirname, 'src/assets/fonts/')
-  }
-}
-
-config.module = {
-  rules: [
-    {
-      test: /\.(js|jsx)$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader'
-      }
-    },
-    {
-      test: /\.html$/,
-      use: {
-        loader: 'html-loader'
-      }
-    },
-    {
-      test: /\.(woff|woff2)$/,
-      type: 'asset/resource',
-      generator: {
-        filename: 'assets/fonts/[name].[contenthash].[ext]'
-      }
-    }
-  ]
-}
-
 module.exports = (env, argv) => {
-  if (argv.mode === 'development') {
-    config.mode = 'development'
+  const isProduction = argv.mode === 'production'
 
-    config.devServer = {
-      port: 2303,
-      contentBase: path.join(__dirname, 'dist'),
-      compress: true,
-      historyApiFallback: true
+  config.entry = './src/index.js'
+
+  config.output = {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'javascript/[name].[contenthash].js',
+    assetModuleFilename: 'assets/images/[hash][ext]'
+  }
+
+  config.resolve = {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      '@styles': path.resolve(__dirname, 'src/styles/'),
+      '@images': path.resolve(__dirname, 'src/assets/images/'),
+      '@fonts': path.resolve(__dirname, 'src/assets/fonts/')
     }
+  }
 
-    config.module.rules.push({
-      test: /\.(css|pcss|sss)$/,
-      use: [
-        'style-loader',
-        {
-          loader: 'css-loader',
-          options: {
-            importLoaders: 1
-          }
-        },
-        'postcss-loader'
-      ]
-    })
+  config.devServer = isProduction
+    ? {}
+    : {
+        port: 2303,
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        historyApiFallback: true
+      }
 
-    config.module.rules.push({
-      test: /\.(jpe?g|png|webp)$/i,
-      use: [
-        {
-          loader: 'responsive-loader',
-          options: {
-            adapter: require('responsive-loader/sharp'),
-            disable: true
-          }
-        },
-        'webp-loader'
-      ]
-    })
-
-    config.plugins = [
-      new HtmlWebpackPlugin({
-        template: './public/index.html',
-        filename: 'index.html',
-        inject: true
-      }),
-      new MiniCssExtractPlugin({
-        filename: '[name].[contenthash].css'
-      }),
-      new CleanWebpackPlugin(),
-      new Dotenv(),
-      new BundleAnalyzerPlugin({
-        openAnalyzer: false,
-        analyzerMode: 'json'
-      })
+  config.module = {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader'
+        }
+      },
+      {
+        test: /\.(css|pcss|sss)$/i,
+        use: [
+          isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          'postcss-loader'
+        ]
+      },
+      {
+        test: /\.(jpe?g|png|webp)$/i,
+        use: [
+          {
+            loader: 'responsive-loader',
+            options: {
+              adapter: require('responsive-loader/sharp'),
+              disable: isProduction ? false : true,
+              outputPath: 'assets/images'
+            }
+          },
+          'webp-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/fonts/[name].[contenthash].[ext]'
+        }
+      }
     ]
   }
 
-  if (argv.mode === 'production') {
-    config.mode = 'production'
+  config.plugins = [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+      inject: true
+    }),
 
-    config.optimization = {
-      minimize: true,
-      minimizer: [new TerserWebpackPlugin()]
-    }
+    new MiniCssExtractPlugin({
+      filename: 'styles/[name].[contenthash].css'
+    }),
 
-    config.module.rules.push({
-      test: /\.(css|pcss|sss)$/,
-      use: [
-        MiniCssExtractPlugin.loader,
-        {
-          loader: 'css-loader',
-          options: {
-            importLoaders: 1
-          }
-        },
-        'postcss-loader'
-      ]
+    new CleanWebpackPlugin(),
+
+    new Dotenv(),
+
+    new BundleAnalyzerPlugin({
+      analyzerMode: isProduction ? 'disabled' : 'json'
     })
+  ]
 
-    config.module.rules.push({
-      test: /\.(jpe?g|png|webp)$/i,
-      use: [
-        {
-          loader: 'responsive-loader',
-          options: {
-            adapter: require('responsive-loader/sharp')
-          }
-        },
-        'webp-loader'
-      ]
-    })
-
-    config.plugins = [
-      new HtmlWebpackPlugin({
-        template: './public/index.html',
-        filename: 'index.html',
-        inject: true
-      }),
-      new MiniCssExtractPlugin({
-        filename: '[name].[contenthash].css'
-      }),
-      new CleanWebpackPlugin(),
-      new Dotenv()
-    ]
+  config.optimization = {
+    minimize: isProduction ? true : false,
+    minimizer: isProduction ? [new TerserWebpackPlugin()] : []
   }
 
   return config
