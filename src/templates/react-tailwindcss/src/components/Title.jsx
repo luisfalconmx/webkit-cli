7a6f46852de1e1e5f8403262cc1a1195bcbfa6c4
luisfalconmx <me@luisfalconmx.com>
2021-04-24 22:14:31 -0500

    feat: tailwindcss jit compiler optimized

--- src/templates/react-tailwindcss/src/components/Title.jsx
+++ src/templates/react-tailwindcss/src/components/Title.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Title = (props) => {
-  return <h1>{props.text}</h1>
+  return <h1 className="text-green-600">{props.text}</h1>
 }
 
 export default Title
--- src/templates/react-tailwindcss/tailwind.config.js
+++ src/templates/react-tailwindcss/tailwind.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  mode: 'jit',
   purge: ['./**/*.html', './**/*.js', './**/*.jsx'],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
